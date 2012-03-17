d179ca86136ef598e08be61a9a9af96bd472c4ab
Will Fuqua <wafuqua@gmail.com>
2012-03-16 23:28:44 -0400

    add Run Language module

--- build.sbt
+++ build.sbt
@@ -3,3 +3,17 @@ name := "scalawag"
 version := "0.1"
 
 scalaVersion := "2.9.1"
+
+libraryDependencies ~= { seq =>
+  val vers = "0.8.7"
+  seq ++ Seq(
+    "net.databinder" %% "dispatch-core" % vers,
+    "net.databinder" %% "dispatch-oauth" % vers,
+    "net.databinder" %% "dispatch-nio" % vers,
+    /* Twine doesn't need the below dependencies, but it simplifies
+     * the Dispatch tutorials to keep it here for now. */
+    "net.databinder" %% "dispatch-http" % vers,
+    "net.databinder" %% "dispatch-tagsoup" % vers,
+    "net.databinder" %% "dispatch-jsoup" % vers
+  )
+}
---
+++ src/main/scala/modules/Run.scala
@@ -0,0 +1,50 @@
+package modules
+import bot.SimpleMessage
+import bot.Module
+import jerklib.Channel
+import dispatch.Http
+import dispatch.url
+import scala.util.parsing.json.JSON
+import jerklib.util.Colors
+import jerklib.events.MessageEvent
+
+class Run extends Module with SimpleMessage {
+
+  val RunCommand = """!run (?<language>[^ ]*) (?<source>.*)""".r
+  val api = "http://api.dan.co.jp/lleval.cgi"
+  
+  val friendlyNames = Map(
+      "python" -> "py",
+      "python3" -> "py3",
+      "ruby" -> "rb",
+      "perl" -> "pl",
+      "javascript" -> "js"
+  )
+    
+    
+  def message(channel:Channel, message:String) {
+    message match {
+      case RunCommand(language, source) => run(channel, language, source)
+      case _ =>
+    }
+  }
+  
+  def run(channel: Channel, language:String, source:String) = {
+    
+    val params = Map(
+      "l" -> friendlyNames.getOrElse(language, language),
+      "s" -> source
+    )
+
+    Http(url(api) <<? params >- { response =>
+	  val json = JSON.parseFull(response).get.asInstanceOf[Map[String, Any]]
+	  val error = json("stderr").asInstanceOf[String]
+	  val output = json("stdout").asInstanceOf[String]
+	   
+	  if(!error.isEmpty)
+	    channel.say(Colors.RED + error.lines.toList.last)
+	  else if (!output.isEmpty)
+	    channel.say(Colors.GREEN + output)
+	})
+  }
+}
